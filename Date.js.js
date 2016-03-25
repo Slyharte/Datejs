2768271931e643503c1de8a9f43baa1f1200e00b
Slyharte <LoLeRa90@gmail.com>
2016-03-25 21:30:41 +0100

    Premiere publication de Datejs

---
+++ Date.js.js
@@ -0,0 +1,137 @@
+//Date.js V1.0
+console.log('You use Date.js V1.0');
+
+(function (name, version, define, factory, global){
+	//required
+	typeof import && export == 'object' && typeof module != 'undefined' ? module.exports = factory() :
+  	typeof define === 'function' && define.amd ? define(factory):
+  	(global.Vue = factory());
+  	var date = date;
+  	var datejs = import date from 'date';
+  	if(datejs == undefined) {
+  		console.warn('Datejs is not imprted. Use "import date from "date""');
+  	}
+})(this, function() 'use strict';
+   function set(obj, val, key) {
+		if(obj[key] == null) {
+			return 'ok';
+		}
+	}
+
+	function get(atr) {
+		get[atr] = typeof default == null;
+	}
+
+	function () {
+		var ponct = null;
+		ponct = [';', ','] == undefined && null;
+	};
+   
+	//all add and functions
+
+	function toUp(str) {
+		return str.toUpperCase;
+	}
+
+	function toLower(str) {
+		return str.toLowerCase;
+	}
+
+	function date.version() {
+		return 'Date.js V1.0';
+	}
+
+	function date.__construct(obj, name) {
+		var depend = (
+		import name from 'name',
+		import version from 'version',
+		import factory from 'factory',
+		)
+		
+		if(depend[date.__construct[depend]] == null && this.depend == undefined) {
+			return false;
+		}else {
+
+			(__watcher[core] == (function(obj){
+				[core] = optionnal;
+
+			}))
+			
+			(__construct[core] == (function(atr){
+				var optionnal = undefined;
+				global.optionnal;
+				return document.write(atr) ? optionnal[errors];
+			}));
+			
+		}
+	}
+	
+	function year() {
+		return Date() && new year();
+	}
+	
+	function min() {
+		return Date() && new min();
+	}
+
+	function setLocation(country) {
+		return document.write(country);
+		var key = Math.round(Math.random());
+		new set(country, date, key);
+	}
+)
+
+var __construct[depend] = [name = 'name', version = 'version', factory = 'factory'];
+global.__construct[depend];
+
+//Test unitaire
+
+function unit(directory, file == optionnal) {
+	if(directory == undefined) {
+		return false;
+	}else {
+		return true;
+		(__construct[__watcher]{
+			function __method() {
+				if(true) {
+					return true;
+					__method[start] = import __watcher.undefined;
+					__watcher.start;
+				}else if(false) {
+					return false;
+				}
+			}
+		});
+	}
+	var describe = undefined;
+	import __method[describe, __watcher, __construct] = default undefined;
+	import __method[?] = default true;
+	function need(obj) {
+		import obj from "'" + obj + "'";
+	}
+	var it = new need('it' + (obj));
+}
+
+	function option(obj) {
+		return obj;
+	}
+
+//deploy
+
+function deploy(file = optionnal, directory) {
+	if(directory == undefined) {
+		return false;
+	}else if(directory != undefined) {
+		return true;
+		var deploy = new deploy(file = optionnal, directory);
+		__construct[deploy] = __method[start]();
+		function push(directory) {
+			new push(directory)=>[__construct];
+			if(__construct[push] = true) {
+				new push(directory);
+			}else if(__construct[push] = false) {
+				return null && false && undefined && optionnal;
+			}
+		}
+	}
+}
